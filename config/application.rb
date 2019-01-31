3f8aebff334c8028fc5bab8f6a0bd8c14d0070d4
Elias Ortega <eliass_56@hotmail.com>
2019-01-30 22:40:57 -0500

    assests preconpile

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module AnimalesFelices
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    # Initialize configuration defaults for originally generated Rails version. 
     config.load_defaults 5.2
 
     config.iva = 0.12
