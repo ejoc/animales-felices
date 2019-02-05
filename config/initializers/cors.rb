4120f969074274c2e61504b249d4309a89f20c9a
Elias Ortega <eliass_56@hotmail.com>
2019-02-05 14:31:37 -0500

    gitignore

--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
-
-#     resource '/assets/*', headers: :any, methods: [:get]
-
-#     resource '/packs/*', headers: :any, methods: [:get]
-#   end
-# end
