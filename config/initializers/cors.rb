3988d578de44506196c26d33264255eaadc5a44d
Elias Ortega <eliass_56@hotmail.com>
2019-02-05 11:22:34 -0500

    remove cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*'
 
-    resource '/assets/*', headers: :any, methods: [:get]
+#     resource '/assets/*', headers: :any, methods: [:get]
 
-    resource '/packs/*', headers: :any, methods: [:get]
-  end
-end
+#     resource '/packs/*', headers: :any, methods: [:get]
+#   end
+# end
